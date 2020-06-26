3e2b5b252f90e581157777b8275b2feb1114b727
Aleksander Babkin <aleksanderbabkin@ababkin.local>
2020-06-26 23:43:57 +0300

    0.0.1

---
+++ README.md
@@ -0,0 +1,23 @@
+## Linezz
+
+Simple, fast and light tool for counting code lines in entire directory (with subdirs).
+
+**Compatible with Node.js 10.x +**
+
+### Install
+
+```
+npm install -g linezz
+```
+
+### Use
+
+```
+linezz ./some-dir
+```
+
+Or use **npx** instead
+
+```
+npm linezz ./some-dir
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+const readline = require('readline')
+const path = require('path')
+
+function countLinesInFile(filePath) {
+  let count = 0
+
+  return new Promise((resolve) => {
+    fs.createReadStream(filePath, { autoClose: true })
+      .on('data', (chunk) => {
+          for (let i = 0; i < chunk.length; ++i) {
+            if (chunk[i] == 10) {
+              count++
+            }
+          }
+      })
+      .on('end', () => {
+        resolve(count)
+      })
+  })
+}
+
+function readFilesInDirectory(dir) {
+  let count = 0
+
+  return new Promise((resolve) => {
+    fs.readdir(dir, { withFileTypes: true }, async (err, dirents) => {
+      if (err) {
+        throw err
+      }
+
+      for (let i = 0; i < dirents.length; i++) {
+        const dirent = dirents[i]
+        const pathname = path.resolve(__dirname, dir, dirent.name)
+
+        if (dirent.isDirectory()) {
+          count += await readFilesInDirectory(pathname)
+        } else {
+          count += await countLinesInFile(pathname)
+        }
+      }
+
+      resolve(count)
+    })
+  })
+}
+
+async function count() {
+  const [dir] = process.argv.slice(2)
+  const before = Date.now()
+
+  if (!dir) {
+    console.error('No directory provided')
+
+    return process.exit(1)
+  }
+
+  const totalCount = await readFilesInDirectory(path.resolve(__dirname, dir))
+
+  console.log('Total lines count:', totalCount)
+  console.log('time', Date.now() - before)
+
+  process.exit()
+}
+
+count()
